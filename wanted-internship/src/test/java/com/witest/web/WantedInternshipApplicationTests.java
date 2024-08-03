0cc368b14084016c6250e84d6ea43a6e8ac64547
lrive <dlwldms9803@gmail.com>
2024-08-03 20:55:33 +0900

    feat: 프로젝트 생성

---
+++ wanted-internship/src/test/java/com/witest/web/WantedInternshipApplicationTests.java
@@ -0,0 +1,13 @@
+package com.witest.web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WantedInternshipApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
